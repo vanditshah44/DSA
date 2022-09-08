791d3604556766bf523d7137154588828b6d64a5
Vandit Shah <vanditshah44@gmail.com>
2022-09-09 01:28:59 +0530

    Binary Search in Rotated Sorted Array

---
+++ BinarySearch/InterviewQuestions/leetcode33.java
@@ -0,0 +1,66 @@
+package BinarySearch.InterviewQuestions;
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+public class leetcode33 {
+    public static void main(String[] args) {
+        
+    }
+
+    static int search(int[]  nums, int target ) {
+        int pivot = findPivot(nums);
+
+        if (pivot == -1 ) {
+            // performing the normal Binary Search 
+            return BinarySearch(nums, target, 0, nums.length -1);
+
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return BinarySearch(nums, target, 0, pivot -1);
+        }
+        if (target <= nums[0]) {
+            return BinarySearch(nums, target, pivot + 1, nums.length -1);
+        }
+        return -1;
+    }
+    static int BinarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start <= end) {
+            int mid = start + (end -start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid+1]) {
+                return mid;
+
+            }  if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    
+}
